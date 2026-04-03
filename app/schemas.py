89fcf0ec6bf263e61f352d7ce4190af77375f82d
eminagracanin-cloud <eminagracanin@icloud.com>
2026-04-03 10:29:54 +0200

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -4,4 +4,6 @@ from pydantic import BaseModel
 class LeadInput(BaseModel):
     total_visits: int
     time_spent_on_website: float
-    lead_source: str
+    page_views_per_visit: float
+    asymmetrique_activity_score: float
+    asymmetrique_profile_score: float
