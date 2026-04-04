3a77e096dffa95fb82fcda513d300fb9f885a46e
eminagracanin-cloud <eminagracanin@icloud.com>
2026-04-04 08:53:41 +0200

    Update preprocess.py

--- pipeline/preprocess.py
+++ pipeline/preprocess.py
@@ -1,10 +1,16 @@
-import pandas as pd
+def preprocess_data(df):
+    df = df[[
+        "TotalVisits",
+        "Total Time Spent on Website",
+        "Page Views Per Visit",
+        "Asymmetrique Activity Score",
+        "Asymmetrique Profile Score",
+        "Converted"
+    ]]
 
-
-def preprocess_data(df: pd.DataFrame):
-    df = df.copy()
-
-    # remove missing values for now
     df = df.dropna()
 
-    return df
+    X = df.drop("Converted", axis=1)
+    y = df["Converted"]
+
+    return X, y
